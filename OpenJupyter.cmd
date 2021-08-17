7bee2b4647073420b816b528c65942735e3e8da7
Honot0 <witalika.f3@gmail.com>
2021-08-17 22:41:09 +0300

    Add files via upload

---
+++ OpenJupyter.cmd
@@ -0,0 +1,21 @@
+cd..
+cd..
+cd..
+cd..
+cd..
+cd..
+cd..
+cd..
+cd..
+
+cd Users
+cd wital
+cd AppData
+cd Local
+cd Programs
+cd Python
+cd Python39
+cd Lib
+cd site-packages
+cd jupyterlab
+py  __main__.py
---
+++ install Jupyter And ALL.cmd
@@ -0,0 +1,20 @@
+cd..
+cd..
+cd..
+cd..
+cd..
+cd..
+cd..
+cd..
+cd..
+
+py -m pip install jupyterlab
+
+py -m pip install pandas 
+
+py -m pip install tensorflow
+py -m pip install matplotlib
+py -m pip install opencv-python
+
+py -m pip install seaborn 
+py -m pip install -U scikit-learn
